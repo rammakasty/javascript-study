97aada7f7cd5ad430b90e2b462536966a35c790a
choi ha young <gkdudk15@naver.com>
2023-05-23 12:49:11 +0900

    feat: 조건문 기본예제

---
+++ 11.js
@@ -0,0 +1,24 @@
+// 조건문 - if, else if, else, switch
+// 1. if문
+let x = -1
+
+// 1-1
+// if (x < 11) {
+//     console.log("x는 양수입니다");
+// }
+// // 조건이 false이기에 실행x
+// // 1-2
+// let y = "hello world"
+
+// if (y.length >= 5) {
+//     console.log(y.length);
+// }
+
+// if - else if - else 문
+if (x < 0) {
+    console.log("1");
+} else if (x >= 0 && x < 10) {
+    console.log("2");
+} else {
+    console.log("3");
+}
